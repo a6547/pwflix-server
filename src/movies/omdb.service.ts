71ad46a537d0af715b5bdd64516552260093311e
Filipe Vilas-Boas <filipemvb@gmail.com>
2022-03-29 21:39:10 +0100

    Update logs

--- src/movies/omdb.service.ts
+++ src/movies/omdb.service.ts
@@ -2,20 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 import axios from 'axios';
 
-
 const OMDb_KEY = '9ed2556f';
 const searchedResults = {};
 
 @Injectable()
 export class OmdbService {
   private searchMovies(searchText: string) {
-    console.log('calling api');
     const url = `https://www.omdbapi.com/?apikey=${OMDb_KEY}&s=${searchText}`;
     return axios(url).then((response) => response.data.Search ?? []);
   }
 
   async find(text: string) {
     if (!(text in searchedResults)) {
+      console.log(`Getting results for ${text} from api`);
       const result = await this.searchMovies(text);
       searchedResults[text] = result.map((item) => {
         const movie = new Movie();
@@ -26,6 +25,8 @@ export class OmdbService {
         movie.type = item.Type;
         return movie;
       });
+    } else {
+      console.log(`Getting results for ${text} from cache`);
     }
     return searchedResults[text];
   }
